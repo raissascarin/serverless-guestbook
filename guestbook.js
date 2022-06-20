c2c9aac7bdcf711f4eeae533e4918633bd54c0b5
Raíssa Scarin <106627461+raissascarin@users.noreply.github.com>
2022-06-20 11:30:43 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = https://1f96d733.eu-de.apigw.appdomain.cloud/guestbook ;
+const apiUrl = "https://1f96d733.eu-de.apigw.appdomain.cloud/guestbook";
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
